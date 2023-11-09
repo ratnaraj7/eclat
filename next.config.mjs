7a0828fa56722c00328b832b15ad4387535d8919
Ratnaraj Daimary <ratnarajdaimary7@gmail.com>
2023-11-09 10:29:03 +0530

    chore: add image source unsplash for dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import './env.mjs';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = (() => {
+  const remotePatterns = process.env.NODE_ENV === 'development' ? [{ hostname: '*.unsplash.com' }] : [];
+
+  return { images: { remotePatterns } };
+})();
 
 export default nextConfig;
